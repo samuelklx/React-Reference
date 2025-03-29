b1e00affe6f481fd4010322048374e1d0ad74016
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-03-29 00:00:28 -0400

    new file:   #10InstallingNextjs.jsx 	new file:   .gitignore 	new file:   .next/app-build-manifest.json 	new file:   .next/build-manifest.json 	new file:   .next/cache/.rscinfo 	new file:   .next/cache/webpack/client-development/0.pack.gz 	new file:   .next/cache/webpack/client-development/1.pack.gz 	new file:   .next/cache/webpack/client-development/2.pack.gz 	new file:   .next/cache/webpack/client-development/3.pack.gz 	new file:   .next/cache/webpack/client-development/index.pack.gz 	new file:   .next/cache/webpack/client-development/index.pack.gz.old 	new file:   .next/cache/webpack/server-development/0.pack.gz 	new file:   .next/cache/webpack/server-development/1.pack.gz 	new file:   .next/cache/webpack/server-development/2.pack.gz 	new file:   .next/cache/webpack/server-development/index.pack.gz 	new file:   .next/cache/webpack/server-development/index.pack.gz.old 	new file:   .next/package.json 	new file:   .next/react-loadable-manifest.json 	new file:   .next/server/app-paths-manifest.json 	new file:   .next/server/app/page.js 	new file:   .next/server/app/page_client-reference-manifest.js 	new file:   .next/server/interception-route-rewrite-manifest.js 	new file:   .next/server/middleware-build-manifest.js 	new file:   .next/server/middleware-manifest.json 	new file:   .next/server/middleware-react-loadable-manifest.js 	new file:   .next/server/next-font-manifest.js 	new file:   .next/server/next-font-manifest.json 	new file:   .next/server/pages-manifest.json 	new file:   .next/server/server-reference-manifest.js 	new file:   .next/server/server-reference-manifest.json 	new file:   .next/server/vendor-chunks/@swc+helpers@0.5.15.js 	new file:   .next/server/vendor-chunks/next@15.2.4_react-dom@19.1.0_react@19.1.0__react@19.1.0.js 	new file:   .next/server/webpack-runtime.js 	new file:   .next/static/chunks/_app-pages-browser_node_modules_pnpm_next_15_2_4_react-dom_19_1_0_react_19_1_0__react_19_1_0_-8c3ee3.js 	new file:   .next/static/chunks/app-pages-internals.js 	new file:   .next/static/chunks/app/layout.js 	new file:   .next/static/chunks/app/page.js 	new file:   .next/static/chunks/main-app.js 	new file:   .next/static/chunks/polyfills.js 	new file:   .next/static/chunks/webpack.js 	new file:   .next/static/development/_buildManifest.js 	new file:   .next/static/development/_ssgManifest.js 	new file:   .next/static/webpack/1ef514a30a79e9c7.webpack.hot-update.json 	new file:   .next/static/webpack/633457081244afec._.hot-update.json 	new file:   .next/static/webpack/81a5627d7e4f19c8.webpack.hot-update.json 	new file:   .next/static/webpack/a74d40e0cbbb73a7.webpack.hot-update.json 	new file:   .next/static/webpack/app/page.1ef514a30a79e9c7.hot-update.js 	new file:   .next/static/webpack/app/page.81a5627d7e4f19c8.hot-update.js 	new file:   .next/static/webpack/app/page.a74d40e0cbbb73a7.hot-update.js 	new file:   .next/static/webpack/app/page.d57de5932cad3b0f.hot-update.js 	new file:   .next/static/webpack/d57de5932cad3b0f.webpack.hot-update.json 	new file:   .next/static/webpack/webpack.1ef514a30a79e9c7.hot-update.js 	new file:   .next/static/webpack/webpack.81a5627d7e4f19c8.hot-update.js 	new file:   .next/static/webpack/webpack.a74d40e0cbbb73a7.hot-update.js 	new file:   .next/static/webpack/webpack.d57de5932cad3b0f.hot-update.js 	new file:   .next/trace 	new file:   .next/types/app/layout.ts 	new file:   .next/types/app/page.ts 	new file:   .next/types/cache-life.d.ts 	new file:   .next/types/package.json 	modified:   README.txt 	new file:   app/layout.js 	new file:   app/page.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pnpm-lock.yaml 	new file:   pnpm-workspace.yaml
    Changes not staged for commit:
    	modified:   .next/server/app/page.js
    	modified:   .next/trace

---
+++ app/page.js
@@ -0,0 +1,39 @@
+/* Next.js uses a file-based routing system, which means that the file structure of your project determines the routes of your application. The app directory is where you will create your application’s pages.Page.js is what the url will point to, and will load the front end of the app. Each folder points to a new page in the webapp; each corresponds to a route in your application. For example, if you create a file called page.js in the pages directory, it will be accessible at the root URL of your application (e.g., http://localhost:3000/).
+ */
+
+"use client"; // This is a client component, which means it will be rendered on the client side.
+import { useState } from "react";
+function Header({ title }) {
+  console.log(title);
+  return <h1>{title}</h1>;
+}
+
+export default function HomePage() {
+  // export allows a component (function) to be imported into other files. This is a client component, which means it will be rendered on the client side.
+  // default export allows the component to be imported without using curly braces. only one default export is allowed per file.
+  const names = [
+    "Ada Lovelace",
+    "Grace Hopper",
+    "Marie Curie",
+    "Rosalind Franklin",
+  ];
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Like({likes})</button>
+    </div>
+  );
+}
+
+// Now that we have the basic structure of our Next.js application, we can start building out the rest of the application. Next.js provides a lot of features out of the box, such as server-side rendering, static site generation, and API routes. We will cover these features in more detail in the following sections.
+// One cool next.js feature is the ability to run a development server that will automatically reload your application when you make changes to your code. To start the development server, run the following command in your terminal. There is a command to do so in the package.json file, so check that out next.
